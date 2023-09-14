33c212d8b0ef1ae9444f6e0029d4492009cf1c7a
Marconi S. de Jesus <marconi.developer@outlook.com.br>
2023-09-14 17:40:05 -0300

    chapter2: watching GCC preprocessor

---
+++ chapter2-quick-tour/toProcess_processed.c
@@ -0,0 +1,13 @@
+# 0 "toProces.c"
+# 0 "<built-in>"
+# 0 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 0 "<command-line>" 2
+# 1 "toProces.c"
+# 13 "toProces.c"
+int main()
+{
+        500;
+        1;
+        0;
+}
