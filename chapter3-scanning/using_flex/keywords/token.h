58207cc8f70886b743ac33e39262d87532f17a3d
Marconi S. de Jesus <marconi.developer@outlook.com.br>
2023-10-01 04:41:24 -0300

    remove old content

--- chapter3-scanning/using_flex/keywords/token.h
+++
@@ -1,9 +0,0 @@
-typedef enum 
-{
-        TOKEN_EOF=0,
-        TOKEN_LOOP_KEYWORD,       // 1
-        TOKEN_CONDITION_KEYWORD,  // 2
-        TOKEN_DIGIT,              // 3
-        TOKEN_CHAR,               // 4
-        TOKEN_ERROR               // 5
-} Token;
--- chapter3-scanning/using_flex/sample/token.h
+++
@@ -1,9 +0,0 @@
-typedef enum 
-{
-        TOKEN_EOF=0,
-        TOKEN_WHILE,
-        TOKEN_ADD,
-        TOKEN_IDENT,
-        TOKEN_NUMBER,
-        TOKEN_ERROR,
-} token_t;
